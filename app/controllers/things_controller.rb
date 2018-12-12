26413f7bd9635dec1dc486ab69b97f80492bb7e7
jtrtj <jt.r@live.com>
2018-12-12 16:02:42 -0700

    Add has_one_attached :image to thing model, add image to show, form and strong params

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -69,6 +69,6 @@ class ThingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def thing_params
-      params.require(:thing).permit(:title, :artist)
+      params.require(:thing).permit(:title, :artist, :image)
     end
 end
